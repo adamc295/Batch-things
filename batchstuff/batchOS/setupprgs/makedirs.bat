bf539489a022e84d83e51e476450c61c076b9ac4
adamc295 <mstowe584923@gmail.com>
2017-02-18 20:43:43 +0000

    Update makedirs.bat

--- batchstuff/batchOS/setupprgs/makedirs.bat
+++ batchstuff/batchOS/setupprgs/makedirs.bat
@@ -1,6 +1,6 @@
 @echo off
 title making directories
-md C:\Users\Andy.WESTYEM\Contacts\Desktop\stuff\batchstuff\batchOS\OS
-md C:\Users\Andy.WESTYEM\Contacts\Desktop\stuff\batchstuff\batchOS\sys
+md C:\Users\########\Contacts\Desktop\stuff\batchstuff\batchOS\OS
+md C:\Users\########\Contacts\Desktop\stuff\batchstuff\batchOS\sys
 echo done
 exit
